536c9729ec0f35f7420197b6ef7623eacb7c5316
Leonard Lee <archerleonard@gmail.com>
2026-04-01 17:03:12 -0700

    Add: palindrome-partitioning - submission-3

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        if not s:
+            return []
+
+        res = []
+        sub = []
+
+        def helper(idx):
+            if idx == len(s):
+                res.append(sub.copy())
+                return
+
+            for j in range(idx, len(s)):
+                substr = s[idx: j + 1]
+                if self.isPalin(substr):
+                    sub.append(substr)
+                    helper(j + 1)
+                    sub.pop()
+
+        helper(0)
+        return res
+
+    def isPalin(self, s):
+        i, j = 0, len(s) - 1
+
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+        
