756b748534230ecb61d4a44b00ab78353163e8dc
Leonard Lee <archerleonard@gmail.com>
2026-05-23 23:40:38 -0700

    Add: design-word-search-data-structure - submission-12

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-12.py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.isWord = False
+        # map ch: TireNode
+        self.children = {}
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for i in range(len(word)):
+            ch = word[i]
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+
+            cur = cur.children[ch]
+        cur.isWord = True
+
+    def search(self, word: str) -> bool:
+        # key we need to have TrieNode as input
+        def dfs(idx: int, cur: TrieNode) -> bool:
+            if idx == len(word):
+                return cur.isWord
+
+            ch = word[idx]
+            if ch != ".":
+                if ch not in cur.children:
+                    return False
+                cur = cur.children[ch] 
+                return dfs(idx + 1, cur)
+            else:
+                for ch, node in cur.children.items():
+                    if dfs(idx + 1, node):
+                        return True
+                return False
+        return dfs(0, self.root)
+        
