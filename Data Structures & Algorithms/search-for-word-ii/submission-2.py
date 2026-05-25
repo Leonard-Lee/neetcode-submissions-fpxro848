dbdcf0046f88f9f34c5319c73cfb75227c3821c8
Leonard Lee <archerleonard@gmail.com>
2026-05-25 09:55:00 -0700

    Add: search-for-word-ii - submission-2

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-2.py
@@ -0,0 +1,60 @@
+class TrieNode:
+    def __init__(self):
+        self.isWord = False
+        self.children = {}
+        self.word = ""
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        if not board or not board[0]:
+            return []
+
+        self.root = TrieNode()
+
+        def buildWord(word: str) -> None:
+            cur = self.root
+            for ch in word:
+                if ch not in cur.children:
+                    cur.children[ch] = TrieNode()
+                cur = cur.children[ch]
+            cur.isWord = True
+            cur.word = word
+
+        # build the trie tree
+        for word in words:
+            buildWord(word)
+
+        rows = len(board)
+        cols = len(board[0])
+        visitSet = set()
+        res = []
+
+        def dfs(r, c, cur):
+            if r < 0 or r >= rows or c < 0 or c >= cols or (r, c) in visitSet:
+                return
+
+            ch = board[r][c]
+            # key: actually we need to check cur.children
+            if ch not in cur.children:
+                return
+
+            cur = cur.children[ch]
+
+            if cur.isWord:
+                res.append(cur.word)
+                # key: set it to False
+                cur.isWord = False
+
+            visitSet.add((r, c))
+            dfs(r + 1, c, cur)
+            dfs(r - 1, c, cur)
+            dfs(r, c + 1, cur)
+            dfs(r, c - 1, cur)
+            visitSet.remove((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r, c, self.root)
+
+        return res
+        
