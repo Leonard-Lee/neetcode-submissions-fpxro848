d36a96557fa3190e4c73d5f6419be85514fa4e81
Leonard Lee <archerleonard@gmail.com>
2026-05-29 10:47:14 -0700

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,36 @@
+class Solution:
+    # two ways to solve this
+    # dfs
+    # union find
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+
+        # map a node to a list of its neighbors
+        adjMap = defaultdict(list)
+        for n1, n2 in edges:
+            adjMap[n1].append(n2)
+            adjMap[n2].append(n1)
+
+        visitSet = set()
+        def dfs(n, pre) -> bool:
+            if n in visitSet:
+                return False
+
+            visitSet.add(n)
+            for nei in adjMap[n]:
+                if nei != pre:
+                    if not dfs(nei, n):
+                        return False
+            return True
+
+        if not dfs(0, -1):
+            return False
+
+        return len(visitSet) == n 
+
+
+
+
+        
+        
