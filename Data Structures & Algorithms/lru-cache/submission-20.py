8af973882b6a8571adf5bcdc79ac588480249ba3
Leonard Lee <archerleonard@gmail.com>
2026-05-30 09:59:51 -0700

    Add: lru-cache - submission-20

---
+++ Data Structures & Algorithms/lru-cache/submission-20.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, key=0, val=0, pre=None, next=None):
+        self.key = key
+        self.val= val
+        self.pre = pre
+        self.next = next
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        # mapping a key to a double linked list node
+        self.map = {}
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.pre = self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.map:
+            return -1
+
+        node = self.map[key]
+        self.remove(node)
+        self.insertToTail(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.map:
+            self.remove(self.map[key])
+        newNode = Node(key, value)
+        self.map[key] = newNode
+        self.insertToTail(newNode)
+
+    def remove(self, node: Node) -> None:
+        pre = node.pre
+        nxt = node.next
+        pre.next = nxt
+        nxt.pre = pre
+
+        node.pre = None
+        node.next = None
+    
+    def insertToTail(self, node: Node) -> None:
+        preTail = self.tail.pre
+        preTail.next = node
+        node.pre = preTail
+        self.tail.pre = node
+        node.next = self.tail
+
+        if len(self.map) > self.cap:
+            deletedNode = self.head.next
+            self.remove(deletedNode)
+            del self.map[deletedNode.key]
+        
