7cd3754f7703c9f3a12c9fa38424e8fcec781361
Leonard Lee <archerleonard@gmail.com>
2026-06-01 09:34:42 -0700

    Add: word-ladder - submission-4

---
+++ Data Structures & Algorithms/word-ladder/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if not wordList:
+            return 0
+
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return 0
+
+        atoz = "abcdefghijklmnopqrstuvwxyz"
+        queue = deque()
+        queue.append((beginWord, 1))
+
+        while queue and wordSet:
+            word, step = queue.popleft()
+            for i in range(len(word)):
+                for ch in atoz:
+                    newWord = word[:i] + ch + word[i + 1:] 
+                    if newWord == endWord:
+                        return step + 1
+                    if newWord in wordSet:
+                        queue.append((newWord, step + 1))
+                        wordSet.remove(newWord)
+
+        return 0
+
+            
+        
