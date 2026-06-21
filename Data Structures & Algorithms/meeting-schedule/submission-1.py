16cd55d326a51bfe53a40523715c61eb11e9d00b
Leonard Lee <archerleonard@gmail.com>
2026-06-20 19:00:06 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+
+        intervals.sort(key=lambda x: x.start)
+
+        preEnd = intervals[0].end
+        for i in range(1, len(intervals)):
+            start, end = intervals[i].start, intervals[i].end
+            if preEnd > start:
+                return False
+            else:
+                preEnd = end
+
+        return True
+
