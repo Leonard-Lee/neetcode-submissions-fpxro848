0890a358ff603d345c7c04b0e18f53004ecef061
Leonard Lee <archerleonard@gmail.com>
2026-06-29 13:37:58 -0700

    Add: course-schedule-ii - submission-13

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-13.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        preMap = defaultdict(list)
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visitSet = set()
+        res = []
+        cycleSet = set()
+        def dfs(crs: int) -> bool:
+            if crs in cycleSet:
+                return False
+
+            if crs in visitSet:
+                return True
+
+            cycleSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+
+            cycleSet.remove(crs)
+            visitSet.add(crs)
+            res.append(crs)
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return []
+
+        return res
+
+
+
+        
