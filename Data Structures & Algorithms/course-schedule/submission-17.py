e529431ac13c1066c38bcce6e0781c0288b446ce
Leonard Lee <archerleonard@gmail.com>
2026-06-29 13:23:44 -0700

    Add: course-schedule - submission-17

---
+++ Data Structures & Algorithms/course-schedule/submission-17.py
@@ -0,0 +1,36 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
+
+        # map a course to its prerequisites (list)
+        preMap = defaultdict(list)
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        res = []
+        visitSet = set()
+        cycleSet = set()
+        def dfs(crs: int) -> bool:
+            if crs in cycleSet:
+                return False
+
+            if crs in visitSet:
+                return True
+
+            cycleSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            cycleSet.remove(crs)
+            visitSet.add(crs)
+
+            return True
+
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+
+        return True
+        
