ba252066f0b85d5c50b8b6071aa80b56fa8989fe
Leonard Lee <archerleonard@gmail.com>
2026-07-06 11:17:30 -0700

    Add: integer-to-roman - submission-1

---
+++ Data Structures & Algorithms/integer-to-roman/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symbolList = [["M", 1000], ["CM", 900], ["D", 500], 
+        ["CD", 400], ["C", 100],  ["XC", 90], ["L", 50], ["XL", 40], ["X", 10], ["IX", 9], ["V", 5], ["IV", 4], ["I", 1]]
+
+        res = []
+        for sym, base in symbolList:
+            count = num // base
+            if count:
+                res.append(sym * count)
+                num = num % base
+
+            if num == 0:
+                break
+
+        return "".join(res)
+
+        
