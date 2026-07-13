0be4dce68294eaf62b885d164cd980602f4fb0ba
Leonard Lee <archerleonard@gmail.com>
2026-07-13 12:56:13 -0700

    Add: maximum-product-subarray - submission-6

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        globalMax = float("-inf")
+
+        maxVal = 1
+        minVal = 1
+
+        for num in nums:
+            preMax = maxVal
+            maxVal = max(num * maxVal, num * minVal, num)
+            minVal = min(num * preMax, num * minVal, num)
+            # print("(" + str(maxVal) + ", " + str(minVal) + ")")
+
+            globalMax = max(globalMax, maxVal)
+            # print("(" + str(globalMax) + ", " + str(globalMin) + ")")
+    
+        return globalMax
